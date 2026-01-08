d9831ab654323c706c2f70733651be07794ea010
Spring Cheng <scheng148@my.bcit.ca>
2026-01-07 16:00:26 -0800

    Vet copywriting & refactor for production tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gold: {
+          400: '#FFD700',
+          500: '#DAA520',
+          600: '#B8860B'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
